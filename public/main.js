34619d0595781fe760ef84e8728c8442362cc6dd
benjaminj6 <benjamin.d.johnson@icloud.com>
2016-09-19 14:29:46 -0700

    add notification when another client connects

--- public/main.js
+++ public/main.js
@@ -1,20 +1,29 @@
 $(document).ready(function() {
+	var socket = io();
 
 	var input = $('input');
-	var messages = $('#messages');
-
-	function addMessage(message) {
-		messages.append('<div>' + message + '</div>');
-	}
 
 	input.on('keydown', function(event) {
 		if (event.keyCode === 13) {
 			var message = input.val();
 			addMessage(message);
+			socket.emit('message', message);
+			
 			input.val('');
 		}
-
-
 	});
+
+	function addMessage(message) {
+		var messages = $('#messages');
+		messages.append('<div>' + message + '</div>');
+	}
+
+	function displayConnection(message) {
+		$('#users-connected').append('<p>' + message + '</p>');
+	}
+
+	socket.on('message', addMessage);
+
+	socket.on('connection', displayConnection);
 
 });
--- server.js
+++ server.js
@@ -1,7 +1,21 @@
+var socket_io = require('socket.io');
+var http = require('http');
 var express = require('express');
 
 var app = express();
-
 app.use(express.static('public'));
 
-app.listen(process.env.PORT || 8080); 
+var server = http.Server(app);
+var io = socket_io(server);
+
+io.on('connection', function (socket) {
+    console.log('Client connected');
+    socket.broadcast.emit('connection', '{Username} has entered the chatroom');
+    
+    socket.on('message', function(message) {
+    	console.log('Received message', message);
+    	socket.broadcast.emit('message', message)
+    });
+});
+
+server.listen(process.env.PORT || 8080);
